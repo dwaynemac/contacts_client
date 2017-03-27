986a4842c8b92bb4bbd482361855f0aab1847e24
Dwayne Macgowan <dwaynemac@gmail.com>
2017-03-27 14:19:28 +0000

    validate phone length

--- app/models/telephone.rb
+++ app/models/telephone.rb
@@ -12,7 +12,8 @@ class Telephone < ContactAttribute
 
   attr_accessor :category, :value, :public, :primary
 
-  validates :value, format: { with: /^[\(|\d][\d| |\)\-|\.]{6,16}.*\d$/ }
+  validates :value, format: { with: /^[\(|\d][\d| |\)\-|\.]{6,16}.*\d$/ },
+                    length: { maximum: 45 }
 
   def masked?
     value.present? && value.last == '#'
